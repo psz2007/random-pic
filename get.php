4b1a6dfa01e0a920a56954d5fa55ddebc84353b8
psz2007 <panshizhao2007@163.com>
2023-04-05 23:10:06 +0800

    upl

---
+++ get.php
@@ -0,0 +1,10 @@
+<?php
+	$list = "list.json";
+	if(!file_exists($list)){
+		die('list error');
+	}
+	$file = file_get_contents($list);
+	$pics = json_decode($file);
+	$pic = $pics[array_rand($pics)];
+	die(header('Location: $pic'));
+?>
