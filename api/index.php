e60b538fa84e2d401c95f7864a6529cdcedaa9ce
psz2007 <panshizhao2007@163.com>
2023-04-07 21:11:09 +0800

    vercel dpl

--- api/index.php
+++ api/index.php
@@ -1,10 +1,10 @@
 <?php
-	$list = "list.json";
+	$list = "../list.json";
 	if(!file_exists($list)){
 		die('list error');
 	}
 	$file = file_get_contents($list);
 	$pics = json_decode($file);
 	$pic = $pics[array_rand($pics)];
-	die(header('Location: $pic'));
+	die(header('Location: ../$pic'));
 ?>
